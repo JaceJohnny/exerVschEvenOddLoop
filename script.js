acbb629ff9f8b1601e110ca16f57b266ac2dae20
JaceJohnny <jace.johnny@hci.utah.edu>
2021-11-20 05:28:17 -0700

    Completed EvenOddLoop Assignment

---
+++ script.js
@@ -0,0 +1,6 @@
+for(var i = 0; i < 100; i++){
+	console.log(i)
+}
+console.log('odd')
+/*Create a for loop that iterates through 101 numbers (from 0 - 100). If
+ the current iteration is an Odd number, print "Odd" to the console, otherwise print "Even"*/
